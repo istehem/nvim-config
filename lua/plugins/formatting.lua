d9b8ccc3f656240c9e63fc6ec330c1895ca809c2
Ingemarsson Oskar <oskar@oskar.me>
2026-06-21 20:25:36 +0200

    use prettier to format vue files

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters_by_ft = {
+      vue = { "prettierd", "prettier" },
+      javascript = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+    },
+  },
+}
